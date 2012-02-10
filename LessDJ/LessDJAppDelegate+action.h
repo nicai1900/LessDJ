f8a79bd786e74c81ab8d4158fb13417049903f82
xu han <xhan87@gmail.com>
2012-02-10 19:16:13 +0800

    added new delegate category.  removed binding selected index for pop up button of channels.

---
+++ LessDJ/LessDJAppDelegate+action.h
@@ -0,0 +1,13 @@
+//
+//  LessDJAppDelegate+action.h
+//  LessDJ
+//
+//  Created by xu xhan on 2/10/12.
+//  Copyright (c) 2012 xu han. All rights reserved.
+//
+
+#import "LessDJAppDelegate.h"
+
+@interface LessDJAppDelegate (action)
+
+@end
---
+++ LessDJ/LessDJAppDelegate+action.m
@@ -0,0 +1,13 @@
+//
+//  LessDJAppDelegate+action.m
+//  LessDJ
+//
+//  Created by xu xhan on 2/10/12.
+//  Copyright (c) 2012 xu han. All rights reserved.
+//
+
+#import "LessDJAppDelegate+action.h"
+
+@implementation LessDJAppDelegate (action)
+
+@end
